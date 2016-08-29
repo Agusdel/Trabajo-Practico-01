92fcc63b7dbe186645b19f1c7d9d241dc29c5135
DESKTOP-EIONEP7\Agustin <agusdel_94@hotmail.com>
2016-08-29 13:45:55 -0300

    + Unity Proyect - Creado. ! readme - Agregué fuentes.

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed;// = 50f;
+    //public float jumpPower = 150f;
+    public bool grounded;
+
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        speed = 0;
+        grounded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetFloat("Speed", speed);
+        anim.SetBool("Grounded", grounded);        
+        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            speed = 1;
+            grounded = true;
+            Debug.Log("Comienza a correr.");
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad0) || Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            speed = 0;
+            grounded = true;
+            Debug.Log("Se detiene.");
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            grounded = false;
+            Debug.Log("Salta.");
+        }
+    }
+
+    void fixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce((Vector2.right * speed) * h);
+
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Trabajo-Practico-01
 
-Hola Manola
+Sprite Sheet obtenido de: http://www.baypins.com/sonic-sprite-sheets-attack/
+
+Ground obtenido de: http://adf.ly/1B9Ess
