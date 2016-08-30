536463909e95e065b45b6da037c6acaf929d0edf
PC-15\PC01 <agusdel_94@hotmail.com>
2016-08-30 12:21:10 -0300

    ! Cambios

--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -3,26 +3,37 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-    public float speed;// = 50f;
-    //public float jumpPower = 150f;
+    public float speed = 2f;
+    public float jumpPower = 20f;
     public bool grounded;
+    public bool rolling;
+    public bool superRoll;
 
     private Animator anim;
     private Rigidbody2D rb2d;
+    private SpriteRenderer sr;
 
 	// Use this for initialization
 	void Start () {
         anim = gameObject.GetComponent<Animator>();
         rb2d = gameObject.GetComponent<Rigidbody2D>();
-        speed = 0;
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        //speed = 0;
         grounded = true;
+        rolling = false;
+        superRoll = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        anim.SetFloat("Speed", speed);
-        anim.SetBool("Grounded", grounded);        
-        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
+	void Update ()
+    {
+        float horizontalSpeed = Input.GetAxis("Horizontal") * speed;
+        float verticalMovement = Input.GetAxis("Vertical") * jumpPower;
+        anim.SetFloat("Speed", Mathf.Abs(horizontalSpeed));
+        anim.SetBool("Grounded", grounded);
+        anim.SetBool("Rolling", rolling);
+        anim.SetBool("SuperRoll", superRoll);
+        /*if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
         {
             speed = 1;
             grounded = true;
@@ -38,14 +49,45 @@ public class Player : MonoBehaviour {
         {
             grounded = false;
             Debug.Log("Salta.");
+        }*/
+
+
+        rb2d.AddForce(Vector2.right * horizontalSpeed);
+        if (horizontalSpeed < 0)
+            sr.flipX = true;
+        else if (horizontalSpeed > 0)
+            sr.flipX = false;
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rolling = true;
+            grounded = true;
+            Debug.Log("Comienza a correr.");
         }
+        if (horizontalSpeed == 0)
+        {
+            rolling = false;
+            superRoll = false;
+        }
+
+        rb2d.AddForce(Vector2.up * verticalMovement);
+        if (verticalMovement > 0)
+            grounded = false;
+        else
+            grounded = true;
+
+        /*if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            grounded = false;
+        }*/
+        //transform.Translate(h, 0, 0);
     }
 
-    void fixedUpdate()
+    /*void fixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
 
         rb2d.AddForce((Vector2.right * speed) * h);
 
+    }*/
     }
-}
