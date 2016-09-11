cfed0abcb5784bcf208992102cb635143f093f35
DESKTOP-EIONEP7\Agustin <agusdel_94@hotmail.com>
2016-09-11 03:08:27 -0300

    no message

--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Player : MonoBehaviour {
 
-    public float speed = 2f;
+    public float maxSpeed = 18f;
+    public float speed = 20f;
     public float jumpPower = 20f;
     public bool grounded;
     public bool rolling;
@@ -18,8 +20,7 @@ public class Player : MonoBehaviour {
         anim = gameObject.GetComponent<Animator>();
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         sr = gameObject.GetComponent<SpriteRenderer>();
-        //speed = 0;
-        grounded = true;
+        
         rolling = false;
         superRoll = false;
 	}
@@ -28,11 +29,13 @@ public class Player : MonoBehaviour {
 	void Update ()
     {
         float horizontalSpeed = Input.GetAxis("Horizontal") * speed;
-        float verticalMovement = Input.GetAxis("Vertical") * jumpPower;
-        anim.SetFloat("Speed", Mathf.Abs(horizontalSpeed));
+        //float verticalMovement = Input.GetAxis("Vertical") * jumpPower;
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
         anim.SetBool("Grounded", grounded);
         anim.SetBool("Rolling", rolling);
         anim.SetBool("SuperRoll", superRoll);
+
+
         /*if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
         {
             speed = 1;
@@ -53,41 +56,58 @@ public class Player : MonoBehaviour {
 
 
         rb2d.AddForce(Vector2.right * horizontalSpeed);
+
+        //Speed limiting
+        if (rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed,rb2d.velocity.y);
+        }
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+
+        //rb2d.AddForce(Vector2.up * verticalMovement);
+
+        UpdateSprite(horizontalSpeed);//, verticalMovement);
+        if ( (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetButtonDown("Jump")) && grounded)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+
+        //Friction(horizontalSpeed);
+    }
+
+    private void Friction(float horizontalSpeed)
+    {
+        Vector3 easeVelocity = rb2d.velocity;
+        easeVelocity.y = rb2d.velocity.y;
+        easeVelocity.z = 0.0f;
+        easeVelocity.x *= 0.85f;
+
+        //fake friction
+        if (grounded)// && horizontalSpeed == 0)
+        {
+            rb2d.velocity = easeVelocity;
+        }
+    }
+
+    private void UpdateSprite(float horizontalSpeed)//, float verticalMovement)
+    {
+        //ACA VER SI QUEDA MEJOR USANDO VELOCITY U HORIZONTAL SPEED
         if (horizontalSpeed < 0)
             sr.flipX = true;
         else if (horizontalSpeed > 0)
             sr.flipX = false;
+
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             rolling = true;
-            grounded = true;
-            Debug.Log("Comienza a correr.");
         }
-        if (horizontalSpeed == 0)
+        if (rb2d.velocity.x == 0)
         {
             rolling = false;
             superRoll = false;
         }
-
-        rb2d.AddForce(Vector2.up * verticalMovement);
-        if (verticalMovement > 0)
-            grounded = false;
-        else
-            grounded = true;
-
-        /*if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-            grounded = false;
-        }*/
-        //transform.Translate(h, 0, 0);
     }
-
-    /*void fixedUpdate()
-    {
-        float h = Input.GetAxis("Horizontal");
-
-        rb2d.AddForce((Vector2.right * speed) * h);
-
-    }*/
-    }
+}
